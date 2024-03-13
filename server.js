a2315217499a787512b1685259faeb6bffd13459
DBDA d. <DBDA@dypm.org>
2024-03-13 12:59:13 +0530

    new

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Sabji Wala . com</h1>');
+  res.end('<h1>Sabji Wala . com<\n></h1><p>mai hu khan</p>');
 });
 server.listen(7000, () => {
   console.log(`Server running at 7000`);
